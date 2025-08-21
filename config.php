e583d3a5837900cd6b086ffb80b0ed6f49acb101
Omar Farooq <mohammadomarf37@gmail.com>
2025-08-22 03:40:31 +0500

    Update config.php

--- config.php
+++ config.php
@@ -7,11 +7,5 @@ $password = "";
 $database = "db_ecommerce";
 $conn = mysqli_connect($hostname, $username, $password, $database);
 
-// Infinityfree
-// $hostname = "sql309.infinityfree.com";
-// $username = "if0_38149375";
-// $password = "omarfarooq1462";
-// $database = "if0_38149375_db_ecommerce";
-// $conn = mysqli_connect($hostname, $username, $password, $database);
 
 ?>
